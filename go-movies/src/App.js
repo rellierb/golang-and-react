fde6774affa0224e56f97a9c73634cbcb02131f1
rellie balagat <relliebalagat@gmail.com>
2022-08-05 21:53:52 +0800

    add go-movies repository

---
+++ go-movies/src/App.js
@@ -0,0 +1,51 @@
+import React, {Component, Fragment} from 'react';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Movies from './components/Movies';
+import Home from './components/Home';
+import Admin from './components/Admin';
+
+export default function App() {
+  return (
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">Go Watch a Movie!</h1>
+          <hr className="mb-3"></hr>
+        </div>
+
+
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/admin">Manage Catalogue</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col-md-10">
+
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/movies" component={Movies} />
+              <Route path="/admin" component={Admin} />
+            </Switch>
+
+          </div>
+        </div>
+      </div>
+
+
+    </Router>
+    
+  );
+}
+
+
