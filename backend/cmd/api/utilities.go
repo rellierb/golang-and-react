62c876a75c5070dc378c12109e5908211053db1f
rellie balagat <relliebalagat@gmail.com>
2022-08-29 15:54:12 +0800

    connecting the api to jwt tokens

--- backend/cmd/api/utilities.go
+++ backend/cmd/api/utilities.go
@@ -6,6 +6,7 @@ import (
 )
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, wrap string) error {
+
 	wrapper := make(map[string]interface{})
 
 	wrapper[wrap] = data
