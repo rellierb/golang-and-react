cecd98584b7230e964fc8591ed58b3e9d1d97cc3
rellie balagat <relliebalagat@gmail.com>
2022-08-31 20:50:21 +0800

    implementing graphql

--- go-movies/src/components/Home.js
+++ go-movies/src/components/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-
+import Ticket from "./../images/movie_tickets.jpg"
+import "./Home.css";
 
 export default class Home extends Component { 
 
   render() {
-    return (<h2>Home Page</h2>);
+    return (
+      <div className="text-center">
+        <h2>This is the home page</h2>
+        <hr />
+        <img src={Ticket} alt="movie ticket" />
+        <hr />
+        <div className="tickets"></div>
+      </div>
+    );
   }
  
 }
